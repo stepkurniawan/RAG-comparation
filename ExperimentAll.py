515ced1450fed0c16fde2bf607044c00f5f1a323
Stephen Kurniawan <stepkurniawan@gmail.com>
2024-03-12 11:01:52 +0100

    presents the table for experiment all

--- ExperimentAll.py
+++ ExperimentAll.py
@@ -31,8 +31,7 @@ from ragas.metrics import (
 #### fixed
 CHUNK_SIZE = 200 # since we know the lower it is, the better
 CHUNK_OVERLAP_SCALE = 0.1
-# TOP_K = [2,3,4] # these are 3 best topk
-TOP_K = [2]
+TOP_K = [2,3,4] # these are 3 best topk
 QUESTION_DATASET = load_50_qa_dataset()['train']
 FOLDER_PATH ="experiments/ALL/"
 
@@ -113,6 +112,7 @@ VECTORSTORES = [faiss_str]
 # KNOWLEDGE_BASES = [ suswiki_str]
 EMBEDDINGS = [bge_str,]
 INDEX_DISTANCES = [cosine_str, innerproduct_str]
+EVALUATE_FLAG = False
 
 def load_or_create_vectorstore(vector_store_name, vector_store_path, knowledge_base, embedding, index_distance):
     if vector_store_name == faiss_str:
@@ -165,77 +165,163 @@ def run_all(KNOWLEDGE_BASES, EMBEDDINGS, VECTORSTORES, INDEX_DISTANCES, TOP_K, L
                             print("output created in path:", FOLDER_PATH)
                             logger.info(f"output created in path: {FOLDER_PATH}, check for CSV and JSON {language_model.name} in {vector_store_path} ")
 
-                        
-run_all(KNOWLEDGE_BASES, EMBEDDINGS, VECTORSTORES, INDEX_DISTANCES, TOP_K, LLMS, QUESTION_DATASET, FOLDER_PATH)
+
+# uncomment run_all to create a csv and json file. Right now, the ones that are missing are from GPT35
+# run_all(KNOWLEDGE_BASES, EMBEDDINGS, VECTORSTORES, INDEX_DISTANCES, TOP_K, LLMS, QUESTION_DATASET, FOLDER_PATH)
 
 # %% Optional: FIX some answer
 
 
 #%% RAGAS Evaluation JSON dataframe
+def evaluate_json_from_generated_contexts(file_name):
+    """
+    input: json file name
+    output: ragas evaluation result, and save it as csv and json
+    """
+    #### Create dataset from json file
+    # create a dataframe from json file
+    df = pd.read_json(file_name)
+
+    # change column names query -> question, ground_truths -> ground_truth, result -> answer, source_documents -> contexts
+    df = df.rename(columns={'query': 'question', 
+                            'ground_truths': 'ground_truth', 
+                            'result': 'answer', 
+                            'source_documents': 'contexts'})
+    
+    # clean context into just page_content
+    for i in range(len(df)):
+        for j in range(len(df['contexts'][i])):
+            if isinstance(df['contexts'][i][j], dict):
+                df['contexts'][i][j] = df['contexts'][i][j]['page_content']
+    
+    data_dict = {
+        'question': df['question'].tolist(),
+        'ground_truth': df['ground_truth'].tolist(),
+        'contexts': df['contexts'].tolist(),
+        'answer': df['answer'].tolist(),
+    }
+
+    dataset = Dataset.from_dict(data_dict)
+
+    # Evaluate dataset
+    result = evaluate(
+        dataset,
+        metrics = [
+            answer_relevancy,
+            faithfulness,
+            context_recall,
+            context_precision,
+            answer_correctness
+            ]
+    )
+
+    # save result object as pickle dump
+    with open(FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_RagasEval.pkl", 'wb') as f:
+        pickle.dump(result, f)
+
+    # create dataframe and save it to csv and json file
+    result_df = result.to_pandas()
+    result_df.to_csv(FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_RagasEval.csv")
+    result_df.to_json(FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_RagasEval.json")
+    print(f"evaluation result saved in {FOLDER_PATH + language_model.name}_{vector_store_name_experiment_file}_{k}_eval.csv")
+
+    return result
+
+if EVALUATE_FLAG:
+    for knowledge_base in KNOWLEDGE_BASES:
+            for embedding in EMBEDDINGS:
+                for vector_store_name in VECTORSTORES:
+                    for index_distance in INDEX_DISTANCES:
+                        # Iterate over all values of k in TOP_K
+                        for k in TOP_K:
+                            # Iterate over all language models in LLMS
+                            for language_model in LLMS:
+                                FOLDER_PATH = f"experiments/ALL/{knowledge_base}/{embedding}/{vector_store_name}/{index_distance}/"
+                                if vector_store_name == faiss_str:
+                                    vector_store_name_experiment_file = "FAISS"
+                                elif vector_store_name == chroma_str:
+                                    vector_store_name_experiment_file = "Chroma"
+
+                                file_name = FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_gen.json"
+
+                                ### evaluate json 
+                                
+                                evaluate_json_from_generated_contexts(file_name)
+                                
+
+
+
+
+#%% presents the results
+
+# create a result dataframe
+all_result_df = pd.DataFrame()
+
 for knowledge_base in KNOWLEDGE_BASES:
-        for embedding in EMBEDDINGS:
-            for vector_store_name in VECTORSTORES:
-                for index_distance in INDEX_DISTANCES:
-                    # Iterate over all values of k in TOP_K
-                    for k in TOP_K:
-                        # Iterate over all language models in LLMS
-                        for language_model in LLMS:
-                            FOLDER_PATH = f"experiments/ALL/{knowledge_base}/{embedding}/{vector_store_name}/{index_distance}/"
-                            if vector_store_name == faiss_str:
-                                vector_store_name_experiment_file = "FAISS"
-                            elif vector_store_name == chroma_str:
-                                vector_store_name_experiment_file = "Chroma"
+    for embedding in EMBEDDINGS:
+        for vector_store_name in VECTORSTORES:
+            for index_distance in INDEX_DISTANCES:
+                for k in TOP_K:
+                    for language_model in LLMS:
+                        FOLDER_PATH = f"experiments/ALL/{knowledge_base}/{embedding}/{vector_store_name}/{index_distance}/"
+                        if vector_store_name == faiss_str:
+                            vector_store_name_experiment_file = "FAISS"
+                        elif vector_store_name == chroma_str:
+                            vector_store_name_experiment_file = "Chroma"
 
-                            file_name = FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_gen.json"
+                        # load evaluation result
+                        with open(FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_RagasEval.pkl", 'rb') as f:
+                            result = pickle.load(f)
 
-                            ### evaluate json 
-                            # create a dataframe from json file
-                            df = pd.read_json(file_name)
+                        new_row = pd.DataFrame({
+                            'KB': [knowledge_base],
+                            'Embedding': [embedding],
+                            'Vector Store': [vector_store_name],
+                            'Index Distance': [index_distance],
+                            'k Docs': [k],
+                            'LLM': [language_model.name],
+                            'Answer Relevancy': [round(result['answer_relevancy'], 3)],
+                            'Faithfulness': [round(result['faithfulness'], 3)],
+                            'Context Recall': [round(result['context_recall'], 3)],
+                            'Context Precision': [round(result['context_precision'], 3)],
+                            'Answer Correctness': [round(result['answer_correctness'], 3)]
+                        }) # input of the DataFrame is a dictionary
 
-                            # change column names query -> question, ground_truths -> ground_truth, result -> answer, source_documents -> contexts
-                            df = df.rename(columns={'query': 'question', 
-                                                    'ground_truths': 'ground_truth', 
-                                                    'result': 'answer', 
-                                                    'source_documents': 'contexts'})
-                                                        # clean context into just page_content
-                            for i in range(len(df)):
-                                for j in range(len(df['contexts'][i])):
-                                    if isinstance(df['contexts'][i][j], dict):
-                                        df['contexts'][i][j] = df['contexts'][i][j]['page_content']
-                            
-                            data_dict = {
-                                'question': df['question'].tolist(),
-                                'ground_truth': df['ground_truth'].tolist(),
-                                'contexts': df['contexts'].tolist(),
-                                'answer': df['answer'].tolist(),
+                        # Define the mappings
+                        mappings = {
+                            'KB': {
+                                'sustainability-methods-wiki': 'Suswiki',
+                                'wikipedia': 'Wikipedia'
+                            },
+                            'Vector Store': {
+                                'db_faiss': 'FAISS',
+                                'db_chroma': 'Chroma'
+                            },
+                            'Embedding': {
+                                'bge-large-en-v1.5': 'BGE',
+                                'gte-large': 'GTE',
+                                'UAE-Large-V1': 'UAE'
+                            },
+                            'Index Distance': {
+                                'l2': 'Eucledian',
+                                'cosine': 'Cosine',
+                                'ip': 'Inner Product'
+                            },
+                            'LLM': {
+                                'gpt35': 'GPT-3.5',
+                                'mistral': 'Mistral',
+                                'llama2': 'Llama2'
                             }
+                        }
 
-                            dataset = Dataset.from_dict(data_dict)
+                        # Apply the mappings
+                        for column, mapping in mappings.items():
+                            new_row[column] = new_row[column].map(mapping).fillna(new_row[column])
 
-                            # Evaluate dataset
-                            result = evaluate(
-                                dataset,
-                                metrics = [
-                                    answer_relevancy,
-                                    faithfulness,
-                                    context_recall,
-                                    context_precision,
-                                    answer_correctness
-                                    ]
-                            )
+                        all_result_df = pd.concat([all_result_df, new_row])
+                        all_result_df = all_result_df.reset_index(drop=True)
 
-                            # save result object as pickle dump
-                            with open(FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_RagasEval.pkl", 'wb') as f:
-                                pickle.dump(result, f)
-
-                            result_df = result.to_pandas()
-                            result_df.to_csv(FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_RagasEval.csv")
-                            result_df.to_json(FOLDER_PATH + f"{language_model.name}_{vector_store_name_experiment_file}_{k}_RagasEval.json")
-                            print(f"evaluation result saved in {FOLDER_PATH + language_model.name}_{vector_store_name_experiment_file}_{k}_eval.csv")
-
-
-
-
+                        
 
 
 
